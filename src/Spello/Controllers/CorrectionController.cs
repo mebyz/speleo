26baaf8ccd69affb1f40e2f811489269ef34859c
Mebyz <e.botros@lekiosk.com>
2017-10-01 01:03:18 +0200

    use singleton

--- src/Spello/Controllers/CorrectionController.cs
+++ src/Spello/Controllers/CorrectionController.cs
@@ -7,6 +7,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Collections.Generic;
 using System;
+using Spello.Services;
 
 namespace Spello.Controllers
 {
@@ -43,24 +44,6 @@ namespace Spello.Controllers
     public class CorrectionController : Controller
     {
 
-
-        private static string Correct(string input, string language)
-        {
-            List<SymSpellCompound.suggestItem> suggestions = null;
-
-            //check if input term or similar terms within edit-distance are in dictionary, return results sorted by ascending edit distance, then by descending word frequency     
-			if (SymSpellCompound.enableCompoundCheck) suggestions = SymSpellCompound.LookupCompound(input, language, SymSpellCompound.editDistanceMax); else suggestions = SymSpellCompound.Lookup(input, language, SymSpellCompound.editDistanceMax);
-
-            //display term and frequency
-            foreach (var suggestion in suggestions)
-            {
-                Console.WriteLine( suggestion.term + " " + suggestion.distance.ToString() + " " + suggestion.count.ToString("N0"));
-            }
-
-            return suggestions[0].term;
-            //if (SymSpellCompound.verbose != 0) Console.WriteLine(suggestions.Count.ToString() + " suggestions");
-        }
-
         /// <summary>
         /// Get Spello correction
         /// </summary>
@@ -72,24 +55,9 @@ namespace Spello.Controllers
         [ResponseCache(Duration = 3600)] //1h de cache
         public IActionResult GetCorrection(string text)
         {
-            //set global parameters
-			SymSpellCompound.enableCompoundCheck=true;
-            SymSpellCompound.verbose = 1;
-            SymSpellCompound.editDistanceMax = 1;
-
-            Console.Write("Creating dictionary ...");//neuneu
-            Stopwatch stopWatch = new Stopwatch();
-            stopWatch.Start();
-
-            string path = "fr.txt";  //path when using SymSpellCompound.cs
-            if (!SymSpellCompound.LoadDictionary(path, "", 0, 1)) 
-                Console.Error.WriteLine("File not found: " + Path.GetFullPath(path)); 
-
-            Console.WriteLine("\rDictionary: " + SymSpellCompound.wordlist.Count.ToString("N0") + " words, " + SymSpellCompound.dictionary.Count.ToString("N0") + " entries, edit distance=" + SymSpellCompound.editDistanceMax.ToString() + " in " + stopWatch.ElapsedMilliseconds.ToString() + "ms "/*+ (Process.GetCurrentProcess().PrivateMemorySize64/1000000).ToString("N0")+ " MB"*/);//neuneu
-
             string input = "voiciun essai" ;
-            
-            var correct = Correct(input, "");
+
+            var correct = SymSpell.Correct(input, "");
 
             return Ok("faux: " + input + " >> vrai: " + correct);
         }
